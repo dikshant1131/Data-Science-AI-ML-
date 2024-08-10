c36b0e9af510460c200486bf39513a5ffdf5f6b6
Dikshant <dikshantk187@gmail.com>
2024-08-10 19:56:41 +0530

    Add files via upload

---
+++ Python/basic.py
@@ -0,0 +1,150 @@
+# # program to show output
+
+# # print("Hello world") # to show hello world 
+# # print(378264892)
+
+# '''this
+# is 
+# multi
+# lice
+# comment'''
+
+# # VARIABLES : 
+
+# a = 10     # integer ---> int
+# b = 45.6   # decimal ---> float
+# c = "hello"  # string ---> str
+
+# # print(a)
+# # print(b)
+# # print(c)
+
+# # print(type(b))
+
+# # rules to give variable name : 
+
+# # space , special symbol is not allowed
+# # you can use alphabets (A-Z, a-z), numbers(0-9) and underscore( _ )
+# # numbers at first position is not allowed
+
+# # first_name = "Harpreet" # snake casing
+# # FirstName = "Harman"  # pascal casing
+# # firstName = "Raman"   # camel casing
+
+
+# # a = 10
+# # print(a)
+# # a = "hello"
+# # print(a)
+
+
+# # CASE SENSTIVITY : 
+
+# # a = 10
+# # A = 20
+# # print(a)
+
+# # indentation ---> spacing
+
+# # you should be careful while giving indent at begining of line
+
+# # a = 10
+# # print(a)
+
+# # assinging multiple variables  :
+
+# # a,b,c = 10, "hello",10.5
+# # print(c)
+
+# # a = b = c = 10
+
+# # print(a)
+# # print(b)
+# # print(c)
+
+# # print(a , b , c )
+
+# # print("the value of a is" , a)
+
+# # print("Hello world",end="___")
+# # print("Hello world",end="")
+# # print("Hello world",end="***")
+# # print("Hello world")
+
+# # --------------------------------------------------------
+
+# # TYPE CASTING:
+
+# # type casting is used to change data type of a variable
+
+# # a  = 10 
+# # b = 10.9
+# # c = "hello"
+
+# # print(type(c))
+# # c = int(c)
+# # print(type(c))
+
+# # b = int(b)
+# # print(b)
+
+# # a = float(a)
+# # print(a)
+
+# # c = float(c)
+# # print(c)
+
+# # a = str(a)
+# # print(type(a))
+
+# # b = str(b)
+# # print(b)
+# # print(type(b))
+
+# # ------------------------------------------------------------------
+
+# # boolean datatype (represented by bool)
+
+# a = True
+# b = False
+# # print(a)
+# # print(b)
+
+# # print(type(a))
+# # print(type(b))
+
+# # TYPE CASTING OF BOOLEAN :-
+
+# # a = int(a)
+# # print(a)
+
+# # b = int(b)
+# # print(b)
+
+# # a = float(a)
+# # print(a)
+
+# # b = float(b)
+# # print(b)
+
+# # a = str(a)
+# # print(a)
+
+# # b = str(b)
+# # print(b)
+
+# # a = "hello"
+# # a = bool(a)
+# # print(a)
+
+
+# # SWAPING TWO NUMBERS : 
+
+# a = 10
+# b = 20
+# # temp = a
+# # a = b
+# # b = temp
+# # print(b)
+# a,b = b,a
+# print(b)
