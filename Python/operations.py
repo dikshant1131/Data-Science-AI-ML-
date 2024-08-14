6fe20539a8caa8d0effca744918a8d0dd40fe7eb
Dikshant <dikshantk187@gmail.com>
2024-08-14 20:09:53 +0530

    Add files via upload

---
+++ Python/operations.py
@@ -0,0 +1,159 @@
+# Arithmetic operations :
+
+# add ---> + 
+# print(10+20)    # Note : 10 & 20 are known as operands
+                        # + sign is called operator
+
+# subtract ---> -
+# print(20-10)
+
+# division ---> /
+# print(34/5)
+
+# multiplication ---> *
+# print(5*2)
+
+# floor division ---> //
+# print(34//5)
+
+# exponention(power) ---> **
+# print(2**2)
+# print(3**3)  # 3^3 = 3*3*3
+# print(4*3)   # 4^3 = 4*4*4
+
+# modulus ---> %
+# print(5%2)
+# print(8%3)
+
+# program to add two numbers : 
+
+# num1 = 10
+# num2 = 20
+
+# addition = num1 + num2
+# print(addition)
+
+#---------------------------------------------------------
+
+# ASSIGNMENT OPERATORS : 
+
+# =
+
+# a = 10 
+# print(a)
+
+# +=
+
+# a = 10
+# a += 20    # a = a+20
+# print(a)
+
+# -=
+
+# /=
+
+# *=
+
+# //=
+
+# %=
+
+# **=
+
+#---------------------------------------------------------
+
+# COMPARISON OPERATORS : 
+
+# == (equal)
+
+# print(4==4)
+
+# a = 4
+# b = 4.0
+# print(a==b)
+
+# a = 4
+# b = "4"
+# print(a==b)
+
+# !=  (not equal)
+
+# print(5!=10)
+
+# >
+# print(5>9)
+# print(20>1)
+
+# <
+# <=
+# >=
+
+#---------------------------------------------------------
+
+# LOGICAL OPERATORS : 
+
+# 1) and :
+
+'''a       b      result 
+
+  True    True    True
+  False   True    False
+  True    False   False
+  False   False   False    '''
+
+# 2) or
+
+'''a       b      result 
+
+  True    True    True
+  False   True    True
+  True    False   True
+  False   False   False     '''
+
+# 3) not
+
+''' a       result
+
+    True    False
+    False   True        '''
+
+# a = 10
+# b = 30
+
+# print(a>5 and b>10) 
+# print(a<5 and b>10)
+# print(a>5 and b<10)
+# print(a<5 and b<10)
+
+# print(a>5 or b>10)
+# print(a<5 or b>10)
+# print(a>5 or b<10)
+# print(a<5 or b<10)
+
+# print(not a>b)
+# print(not a<b)
+
+
+# a = 20
+# b=100
+# c = 70
+
+# print((a>c//2 and c>50) or not (a+c>b))
+
+#---------------------------------------------------------
+
+# in OPERATOR : 
+
+# used to check whether the value exist in squence or not
+
+# sentence = "This is a Python class"
+
+# print("python" in sentence)
+
+
+# not in OPERATOR : 
+
+# print("python" not in sentence)
+
+
+
