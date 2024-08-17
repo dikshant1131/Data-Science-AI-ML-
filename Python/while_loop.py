4aa6c0f860c9a2aa3390c88717bfb7486bf09797
Dikshant <dikshantk187@gmail.com>
2024-08-17 17:36:15 +0530

    Add files via upload

---
+++ Python/while_loop.py
@@ -0,0 +1,95 @@
+#  while_loop:
+#  syntax:
+
+# while condition:
+#  statements
+# password=input('enter the password ')
+# while password !='dikshant':
+#     print('incorrect password')
+#     password=input('enter your password  again :')
+# print('you are logged in ')
+# ================================================================================
+# program to print first 30 natural numbers by using while loop:
+# i =1
+# while i<=30:
+#     print(i)
+#     i+=1
+# ================================================================
+#program to print even number in a range by taking  input from user side :
+# num=int(input('enter the  starting numbers : '))
+# num1=int(input('enter the ending number'))
+
+# while  num<=num1:
+#    if num%2==0:
+#     print(num)
+#    num+=1
+# ================================================================================
+# program to print all the characters of string :
+# string='hello world'
+# i=0
+# while i<len(string):
+#     print(string[i])
+#     i+=1
+# =======================================================================
+# program to print all the  elenment of  list :
+# my_list =['apple','banana','pine apple','guava','cherry','grape']
+# i=0
+# while i<len(my_list):
+#     print(my_list[i])
+#     i+=1
+# =======================================================================
+# continue and break:
+# i=1
+# while i<=10:
+#     if i==7:
+#         break
+#     print(i)
+#     i+=1
+
+
+# i=1
+# while i<=10:
+#     i+=1
+#     if i==7:
+#       continue
+  
+#     print(i)
+# ===============================================================================
+# program to print multiple table of a number by taking input 
+# format of table must be :
+# 2X1=2
+# 2x2=4
+# 2x3=6
+# ..........
+# 2x10=20
+# table = int(input('Enter the table number : '))
+# i =1
+# while i<=10:
+#    print(f'{table}x{i}={table*i}')
+#    i+=1
+# ===============================================================================
+
+# progranm to check wether the number is prime or not using loops:
+# num=int(input('enter the number : '))
+# prime=True
+# i=2
+# while i<num:
+#     if num%i==0:
+#         prime=False
+#         break
+#     i+=1
+#     print(f'{{num}} ia not a prime number! ')
+# else:
+#     print(f'{num}is a prime number!')
+# 
+
+
+
+    
+
+          
+
+          
+
+
+          
